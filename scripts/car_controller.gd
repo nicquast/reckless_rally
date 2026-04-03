cb04d4ed88b9801f486268ac06f71a89fc9a4b45
nic <nicquast@proton.me>
2026-04-03 18:03:26 +1100

    add configurable options to Car script

--- scripts/car_controller.gd
+++ scripts/car_controller.gd
@@ -1,13 +1,19 @@
 extends VehicleBody3D
 
-@export
-var center_of_mass_node:Node3D
+## If set will set the center of mass to the position of the given node
+@export var center_of_mass_node:Node3D
 
-@export
-var power = 100
+## deprecated
+@export var power = 100
 
-@export
-var steering_limit = deg_to_rad(30)
+## Defines the limit of wheel rotation for steering in radians
+@export var steering_limit = deg_to_rad(30)
+
+## Defines the amount of power produced by the car as a function of RPM
+@export var power_curve: Curve2D
+
+## Defines the amount of braking pressure applied as a function of the brake input axis
+@export var brake_pressure_curve: Curve2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
